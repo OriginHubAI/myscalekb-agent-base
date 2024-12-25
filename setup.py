a2bfbb3eca865f0bc534ec992f758f52081e6910
Jiacheng Sun <jiachengs@moqi.ai>
2024-12-25 18:06:02 +0800

    bump package to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="myscalekb-agent-base",
-    version="0.1.0",
+    version="0.1.1",
     author="jiachengs",
     author_email="jiachengs@myscale.com",
     description="Base classes for MyScaleKB Agent APP",
